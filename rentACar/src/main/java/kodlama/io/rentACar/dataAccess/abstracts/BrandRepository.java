07f8ad02f7188512d1e6d3a34935dee1160495ab
Fatih Üstün <wistaster@hotmail.com>
2023-02-02 23:39:54 +0300

    added databaseTransactions & response request 5d

--- rentACar/src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
+++ rentACar/src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
@@ -1,10 +1,9 @@
 package kodlama.io.rentACar.dataAccess.abstracts;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import kodlama.io.rentACar.entities.conceretes.Brand;
 
-public interface BrandRepository {
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
 
-	List<Brand> getAll();
 }
